1567010f06085a59625825b3aa8d05711771bf68
WilliamDubé2024 <widub15@ulaval.ca>
2025-02-03 14:14:44 -0500

    lettres

--- equipe14.py
+++ equipe14.py
@@ -1,8 +1,7 @@
 import numpy as np
 from math import e
 import matplotlib.pyplot as plt
-from suiteSn import suiteSn
-
+#from suiteSn import suiteSn
 
 a = np.full((6, 1), 1)
 
@@ -12,14 +11,23 @@ c = a.reshape(1, 6)
 
 d = equipe_num * c
 
+#I, matrice identité de taille 6 × 6.
 I = np.identity(6)
+print(I)
 
+#J, matrice de taille 6 × 6 dont toutes les composantes valent 1.
 J = np.full((6, 6), 1)
+print(J)
 
+#K, la matrice de taille 6 × 6 dont la diagonale est le vecteur b
 K = np.diag(b)
+print(K)
 
+#L, la matrice de taille 6 × 6
 L = 55 * I - J + 2 * np.dot(a, c)
+print(L)
 
+#M, matrice obtenue en remplaçant la 1ère colonne de K par a.
 M = K.copy()
 M[:, 0] = a.flatten()
 
@@ -29,3 +37,6 @@ x = np.linalg.solve(M, a)
 
 M_transpose = M.T
 N = np.linalg.solve(M, M_transpose)
+
+
+print(a, b, c)
