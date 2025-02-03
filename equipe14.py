8b6bc86ba62237a04b739eb7ef31d896f34f3403
mathieugobeil <magob52@ulaval.ca>
2025-02-03 13:38:58 -0500

    Les  variables initiales

--- equipe14.py
+++ equipe14.py
@@ -2,3 +2,30 @@ import numpy as np
 from math import e
 import matplotlib.pyplot as plt
 from suiteSn import suiteSn
+
+
+a = np.full((6, 1), 1)
+
+b = np.arange(1, 7)
+
+c = a.reshape(1, 6)
+
+d = equipe_num * c
+
+I = np.identity(6)
+
+J = np.full((6, 6), 1)
+
+K = np.diag(b)
+
+L = 55 * I - J + 2 * np.dot(a, c)
+
+M = K.copy()
+M[:, 0] = a.flatten()
+
+dd = np.linalg.det(M)
+
+x = np.linalg.solve(M, a)
+
+M_transpose = M.T
+N = np.linalg.solve(M, M_transpose)
